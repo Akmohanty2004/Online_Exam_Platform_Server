3c3410b497c0393ca0d0f93dfd5a974290de29d6
Akmohanty2004 <ashiskumarmohanty738@gmail.com>
2026-07-04 00:33:33 +0530

    path add

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use('/api', limiter);
 
 // CORS
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://exam-conducted-platform.netlify.app'],
   credentials: true,
 }));
 
